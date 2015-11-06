b134400b955890738b82267734a414b16ffcf447
Josef Zacek <info@josefzacek.com>
2015-11-06 22:06:48 +0000

    jQuery used for full screen toggle

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
+$( document ).ready(function() {
+
+  $('#full-screen').click(function() {
+    $("body>div").toggleClass('container container-fluid');
+    $( "body>div>h1" ).toggleClass('text-center hidden');
+  });
+
+});
+
 function LivePreview() {
   var text = document.getElementById('source-text-area').value,
   target = document.getElementById('targetDiv'),
   converter = new showdown.Converter(),
   html = converter.makeHtml(text);
   target.innerHTML = html;
-}
-
-function FullScreen() {
-  $( "body>div" ).removeClass( "container" );
-  $( "body>div" ).addClass( "container-fluid" );
-  $( "body>div>h1" ).removeClass("text-center");
-  $( "body>div>h1" ).css("display", "none");
 }
